d06709c65da08a1713f2cb2dd1e331f08ccc8175
maksimkarpin <maxon.kofe@yandex.ru>
2025-05-23 20:17:50 +0300

    End version 2 Practicum Asynchronous programming

--- Phython_+/PracticumSinglton.py
+++ Phython_+/PracticumSinglton.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod
 import requests
+import asyncio
+import numpy as np
 class Method:
     url = 'https://analitika.woysa.club/images/panel/json/download/niches.php'
     def __init__(self,skip, category):
@@ -21,13 +23,20 @@ class Loader(Method):
     def __init__(self) -> None:
         ...
 
-    def download(self, skip, category):
+    async def download(self, skip, category):
         url = self.url + f"?skip = {skip}&price_min=0&price_max=1060225&up_vy_min=0&up_vy_max=108682515&up_vy_pr_min=0&up_vy_pr_max=2900&sum_min=1000&sum_max=82432725&feedbacks_min=0&feedbacks_max=32767&trend=false&sort=sum_sale&sort_dir=-1&id_cat={category}"
         response = requests.get(url)
-        return requests.get(url), response.content
+        print( requests.get(url), response.content)
 
 
 if __name__ == '__main__':
     loader = Loader()
-    print(loader.download(100, 1000))
+    skip = 100
+    categories = np.array_split(range(100, 120), 10)
+    df_result = asyncio.get_event_loop() \
+        .run_until_complete(
+        asyncio.gather(*list(map(lambda x: asyncio.ensure_future(loader.download(skip, categories)), categories)))
+    )
+    print(df_result)
+
 
