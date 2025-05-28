69d9d89feed44e4039bee7e9d689655b32bb077b
maksimkarpin <maxon.kofe@yandex.ru>
2025-05-28 20:33:40 +0300

    End version PracticumQSL 2 part без пароля и с моими вводными сессии.

--- ORM/PractucumSQLAlchemy.py
+++ ORM/PractucumSQLAlchemy.py
@@ -1,15 +1,17 @@
-import sqlalchemy.types
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.orm import DeclarativeBase
 from typing import Annotated
-from sqlalchemy import text
+from sqlalchemy import text, BIGINT
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, VARCHAR
 from sqlalchemy import Column, REAL , SmallInteger
+from abc import abstractmethod
+import pandas as pd
+import requests
+from io import BytesIO
 import datetime
-from sqlalchemy.dialects.postgresql import ARRAY
-
+from sqlalchemy import create_engine
+from pydantic import BaseModel
 
 class Connection:
     def __init__(self, sql_type, user, password, server, port = None, **args) -> None:
@@ -57,7 +59,7 @@ session = SessionBuilder(
         server="localhost",
         port = 5432 ,
         user = 'postgres',
-        password = 'password',
+        password = 'Azlk.21412',
         db_name = 'Teach',
         sql_type = "PostgresSQL"
     )
@@ -80,6 +82,7 @@ class Orders(BaseTable):
     ShipRegion: Mapped[str] = mapped_column(String(15))
     ShipPostalCode: Mapped[str] = mapped_column(String(10))
     ShipCountry: Mapped[str] = mapped_column(String(10))
+    Order: Mapped[list["Products"]] = relationship(back_populates="ProductID")
 
 class Suppliers(BaseTable):
     __tablename__ = 'Suppliers'
@@ -101,19 +104,61 @@ class Suppliers(BaseTable):
 class Products(BaseTable):
     __tablename__ = 'Products'
 
-    ProductID: Mapped[SmallInteger] = mapped_column(SmallInteger,primary_key=True, autoincrement=True)
+    ProductID: Mapped[BaseTable.type_annotation_map['IID']]
     ProductName: Mapped[str] = mapped_column(String(40))
-    SupplierID: Mapped[SmallInteger] = mapped_column(SmallInteger)
-    CategoryID: Mapped[SmallInteger] = mapped_column(SmallInteger)
+    SupplierID: Mapped[int] = mapped_column(SmallInteger)
+    CategoryID: Mapped[int] = mapped_column(SmallInteger)
     QuantityPerUnit: Mapped[str] = mapped_column(String(20))
-    UnitPrice: Mapped[bool] = mapped_column(REAL)
-    UnitsInStock: Mapped[SmallInteger] = mapped_column(SmallInteger)
-    UnitsOnOrder: Mapped[SmallInteger] = mapped_column(SmallInteger)
-    ReorderLevel: Mapped[SmallInteger] = mapped_column(SmallInteger)
+    UnitPrice: Mapped[float]
+    UnitsInStock:Mapped[int] = mapped_column(SmallInteger)
+    UnitsOnOrder: Mapped[int] = mapped_column(SmallInteger)
+    ReorderLevel: Mapped[int] = mapped_column(SmallInteger)
     Discontinued: Mapped[int]
-
-
-
+    Product:Mapped[list["Products"]] = relationship(back_populates="SupplierID")
 
 BaseTable.metadata.create_all(session.engine)
-#BaseTable.metadata.drop_all(session.engine)
+
+
+class Method:
+    url = 'https://analitika.woysa.club/images/panel/json/download/niches.php'
+    def __init__(self,skip, category):
+        self.skip = skip
+        self.category = category
+    @abstractmethod
+    def download(self, skip, category):
+        ...
+
+class DownloadSQLProduct(Method):
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'inst'):
+            cls.obj = range(0, 1000)
+            cls.inst = super(DownloadSQLProduct, cls).__new__(cls)
+        return cls.inst
+
+    def __init__(self):
+        ...
+
+    def download_SQL_product(self,skip, category):
+        url = self.url + f"?skip={skip}&price_min=0&price_max=1060225&up_vy_min=0&up_vy_max=108682515&up_vy_pr_min=0&up_vy_pr_max=2900&sum_min=1000&sum_max=82432725&feedbacks_min=0&feedbacks_max=32767&trend=false&sort=sum_sale&sort_dir=-1&id_cat={category}"
+        response = requests.get(url)
+        df = pd.read_excel(BytesIO(response.content), engine='openpyxl')
+        df['category_id'], df['download_date'] = category, datetime.datetime.now()
+        engine = create_engine(f"postgresql://postgres:Azlk.21412@localhost:5432/Teach")
+        connection1 = engine.connect()
+        df.to_sql('Product', connection1, if_exists='replace', index=False)
+        rs = session.buid().execute(text('select * from "Product"'))
+        #for record in rs:
+            #print(record)
+DownProduct = DownloadSQLProduct()
+DownProduct.download_SQL_product(100, 10000)
+
+class ProductODT(BaseModel):
+    SKU: int
+
+
+
+
+rs = session.buid().execute(text('select * from "Product"')).all()
+for rwo in rs:
+    #print(rwo)
+    print(ProductODT.model_validate(rwo, from_attributes=True))
