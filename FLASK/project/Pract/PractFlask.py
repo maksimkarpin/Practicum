7b81b18f83c89958458acf1d271893d23aad47f7
maksimkarpin <maxon.kofe@yandex.ru>
2025-06-10 00:47:20 +0300

    PracticumFlask_2_lesson.

--- FLASK/project/Pract/PractFlask.py
+++ FLASK/project/Pract/PractFlask.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def lesson1():
     return render_template('index1.html')
+
+@app.route('/lesson2')
+def lesson2():
+    return render_template('index2.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
