ce182e79d7c87890489a5e89e71cc515da0bc468
maksimkarpin <maxon.kofe@yandex.ru>
2025-06-10 00:58:34 +0300

    PracticumFlask_2_lesson!

--- FLASK/project/Pract/PractFlask.py
+++ FLASK/project/Pract/PractFlask.py
@@ -10,5 +10,9 @@ def lesson1():
 def lesson2():
     return render_template('index2.html')
 
+@app.route('/lesson3')
+def lesson3():
+    return render_template('index3.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
