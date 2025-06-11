3386448387c38e74e683a7789288a6802bea7186
maksimkarpin <maxon.kofe@yandex.ru>
2025-06-11 16:54:57 +0300

    PracticumFlask_3_lesson.

--- FLASK/project/Pract/PractFlask.py
+++ FLASK/project/Pract/PractFlask.py
@@ -10,9 +10,34 @@ def lesson1():
 def lesson2():
     return render_template('index2.html')
 
-@app.route('/lesson3')
+@app.route('/lesson3/st')
+def lesson3st():
+    return render_template('notes.html')
+
+@app.route('/lesson3' , methods=["POST"])
 def lesson3():
-    return render_template('index3.html')
+    heading = request.form["heading"]
+    text = request.form["text"]
+    note = {}
+    note.clear()
+    new_note = {heading: text}
+    note.update(new_note)
+    for tx in note:
+        t = tx
+    return render_template('notes.html',t=t,note=note.get(heading))
+
+@app.route('/cart')
+
+
+@app.route('/create')
+def create():
+    return render_template('note.html')
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
